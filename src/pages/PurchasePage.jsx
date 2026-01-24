875023ce39339a29721c8bd645ef6956495ea397
felix-atoma <felixatoma2@gmail.com>
2026-01-24 20:05:02 +0000

    New fix

--- src/pages/PurchasePage.jsx
+++ src/pages/PurchasePage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 // ✅ Import all the child components you are using
-import PurchasePageHero from "../components/PurchaseHero";
-import PurchasePageDetails from "../components/PurchasePageDetails";
+import PurchaseHero from '../components/PurchaseHero'
+import PurchaseDetails from '../components/PurchaseDetails'
 
 export default function PurchasePage() {
   return (
     <div className="min-h-screen">
-      <PurchasePageHero />
-      <PurchasePageDetails />
+      <PurchaseHero />
+      <PurchaseDetails />
     </div>
   );
 }
